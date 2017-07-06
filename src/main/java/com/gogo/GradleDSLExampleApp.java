08e689fa1b32b219f08c8496f755f8b34980c89c
Gautam Trivedi <gtrivedi@gogoair.com>
2017-07-06 09:53:07 -0600

    Added files

---
+++ src/main/java/com/gogo/GradleDSLExampleApp.java
@@ -0,0 +1,32 @@
+package com.gogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by gtrivedi on 7/6/17.
+ */
+
+@SpringBootApplication
+@ComponentScan
+public class GradleDSLExampleApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleDSLExampleApp.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurerAdapter adapter() {
+        return new WebMvcConfigurerAdapter() {
+
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+            }
+        };
+    }
+
+}
